9f069df88866c80db7b2361af86a8d55445d71d0
Bogdan_Ishcenko <bogdan.ishchenko.852@gmail.com>
2024-09-13 20:15:30 +0300

    Strict mode

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
 
 import App from './components/app'
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
-root.render(<App />)
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
